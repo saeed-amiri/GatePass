7aae79f921452766e25c8f768fd43f821cbda256
saeed amiri <sdramiri952@gmail.com>
2025-05-25 19:19:41 +0200

    Dart!

---
+++ DART/primary/variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = 'Saeed';
+  int age = 40;
+  double weight = 85;
+  bool isLearning = true;
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Weight: $weight');
+  print('Learning Dart: $isLearning');
+}
